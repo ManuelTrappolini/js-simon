e8752dae99799a3bf1a5f368fe0c87aff448f8cf
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-10 15:19:11 +0200

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,48 @@
+console.log('it works');
+
+//Visualizzare in pagina 5 numeri casuali.
+//Da lì parte un timer di 30 secondi.
+//Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+//NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+//BONUS:
+//Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
+//Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
+
+
+//0. Tools
+// variables
+// if/else
+// math.random
+//setInterval
+//fuction
+//for loop
+
+//1. Preparazione dati (Creo variabili)
+let randomNUmbers = document.querySelector('.random_numbers')
+let countDown = document.querySelector('.countdown')
+let seconds = 30;
+let clock = setInterval(countdown, 1000)
+
+
+//2 Raccoglimento dati( faccio eseguire math random sulla variabile random numbers e imposto il counddown di 30 secondi)
+randomNUmbers = Math.floor((Math.random() * 5) + 1);
+console.log(randomNUmbers);
+
+function countdown(){
+    if(seconds === 0){
+        clearInterval(clock)
+    }
+    else{
+        countDown.innerHTML = seconds
+        seconds --;
+    }
+}
+
+
+/* function compareNumbers(array, number){
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (array[i].includes(number))
+    }
+} */
